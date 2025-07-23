1e52e3b88d7f70c3254f8212fe39c9856cd22dd8
leonardolopesb <leonardolopbraga@gmail.com>
2025-07-23 14:43:40 -0300

    Preenchimento das funções com código e comentários

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,11 +1,56 @@
-async function addItem(userCart) {
+// 1- adicionar um item ao carrinho
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
-async function deleteItem(userCart, itemId) {
+// 2- remover uma unidade de item do carrinho
+async function removeItem(userCart, item) {
+    const index = userCart.findIndex((p) => p.name === item.name);
+
+    // se o item não for encontrado
+    if (index == -1)
+        console.log("item não encontrado");
+
+    // se o item for encontrado, diminuir a quantidade ou remover o produto do carrinho
+    else {
+        // se houver mais de um item, diminui a quantidade do item
+        if (userCart[index].quantity > 1)
+            userCart[index].quantity -= 1;
+
+        // se for o último item, remove o produto do carrinho
+        else if (userCart[index].quantity == 1)
+            userCart.splice(index, 1);
+    }
+
+    return;
 }
 
-async function removeItem(userCart, index) {
+// 3- deletar um item do carrinho pelo ID do produto
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
 }
 
+// 4- calcular o valor final do carrinho
 async function calculateTotal(userCart) {
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+    console.log(`🎁\nO valor final do carrinho da Shoppe é: R$${result}`);
 }
+
+// 5- exibir o carrinho
+async function displayCart(userCart) {
+  console.log("\nLista de produtos no carrinho:");
+  
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      }x | Subtotal = ${item.subtotal()}`
+    );
+  });
+}
+
+export { addItem, removeItem, deleteItem, calculateTotal, displayCart };
